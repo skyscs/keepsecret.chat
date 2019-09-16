889688cf168a7412ed9cf8ebd948c0ba1e052dfe
Slava Filipov <skyscs@gmail.com>
2019-09-16 23:17:49 +0200

    Refactoring

--- src/services/WebRTC.js
+++ src/services/WebRTC.js
@@ -104,36 +104,12 @@ class WebRTC {
         // TODO: Move to config
         const configuration = { iceServers: [{
                 urls: [
-                    'stun:stun.l.google.com:19302',
-                    'stun:stun1.l.google.com:19302',
-                    'stun:stun2.l.google.com:19302',
                     'stun:stun3.l.google.com:19302',
-                    'stun:stun4.l.google.com:19302',
                 ]
             }, {
-                url: 'turn:numb.viagenie.ca',
+                urls: ['turn:numb.viagenie.ca'],
                 credential: 'muazkh',
                 username: 'webrtc@live.com'
-            },
-            {
-                url: 'turn:192.158.29.39:3478?transport=udp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
-            },
-            {
-                url: 'turn:192.158.29.39:3478?transport=tcp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
-            },
-            {
-                url: 'turn:turn.bistri.com:80',
-                credential: 'homeo',
-                username: 'homeo'
-            },
-            {
-                url: 'turn:turn.anyfirewall.com:443?transport=tcp',
-                credential: 'webrtc',
-                username: 'webrtc'
             }]
         };
         this.rtcConnection = new RTCPeerConnection(configuration)
@@ -185,8 +161,6 @@ class WebRTC {
                 return this.rtcConnection.setLocalDescription(answer)
             })
             .then(() => {
-                console.log('A', createdAnswer)
-
                 this.sendToWs({
                     type: 'answer',
                     answer: createdAnswer,
@@ -197,8 +171,6 @@ class WebRTC {
                 this.dispatch(setConnectedUserId(userId))
                 this.dispatch(setConnectedUserName(connectedUserName))
             })
-
-
             .catch((err) => {
                 console.log(err)
             })
